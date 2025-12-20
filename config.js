a219a4922b54ac4526c865566e2711ea063c0f3b
ceva20 <117743354+ceva20@users.noreply.github.com>
2025-12-20 14:04:38 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.WEDDING_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbyNKgNoHHorGhNSRcawgmEP_3uARf5BS82EniV3mHOSAMIDbOSQswcOhuEi52doQjg0kA/exec",
+  PAY: {
+    intestatario: "Christian Evangelista",
+    iban: "IT40P0306939481100000002400",
+    banca: "Intesa San Paolo s.p.a",
+    note: "Inserisci la causale ESATTAMENTE come indicato."
+  }
+};
